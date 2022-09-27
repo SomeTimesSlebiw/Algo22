3542b75cf243f26706411f92bd907f513629aa39
SomeTimesSlebiw <tanapau5777@gmail.com>
2022-09-27 19:13:38 +0700

    Copy data dokumen

---
+++ Luassegitiga.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int a,t,luas;
+	cout<<"Masukkan alas : ";cin>>a;
+	cout<<"Masukkan tinggi : ";cin>>t;
+	luas = a*t/2;
+	cout<<"Luas segitiga : "<<luas;
+}
